c0cadb942383e8ff63e6a37d71c6ad8c9041e305
seva <vyslezhivayu@gmail.com>
2026-01-30 13:01:06 +0100

    cached data

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,10 +18,10 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
     id("com.android.application") version "8.2.1" apply false
     // START: FlutterFire Configuration
-    id("com.google.gms.google-services") version("4.3.15") apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
     // END: FlutterFire Configuration
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
