471babfd0e79c88d3de8ffc79821521295b12a87
leonmarq <leon@trainvent.com>
2026-08-14 14:08:49 +0200

    trying to fix this shit

--- website/app/page.js
+++ website/app/page.js
@@ -66,7 +66,7 @@ export default function HomePage() {
               const camera = spline.findObjectByName('Camera');
 
               if (camera) {
-                camera.state = 'Base';
+                camera.state = 'Base State';
               }
             }}
           />
