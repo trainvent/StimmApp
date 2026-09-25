cf09722bc84c12f835424be0f210295e131e526f
leonmarq <leon@trainvent.com>
2026-09-25 12:19:35 +0200

    update

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,7 +19,7 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.12.0" apply false
+    id("com.android.application") version "8.12.1" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
